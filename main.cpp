5304af51058a37d864e8d0cbe992c367a1f9c076
Jakub <jakub.jakubecky123@gmail.com>
2021-01-19 13:59:44 +0100

    tmp

--- main.cpp
+++ main.cpp
@@ -70,41 +70,6 @@ int main(){
         nodes.push_back(createdNode);
         //cout<<sizeof(points.back());
     }
-    
-    sort(points.begin(), points.end(), [](const Point p1, const Point p2){
-        if(p1.x != p2.x)
-            return p1.x < p2.x;
-        return p1.y < p2.y;
-    });
-
-    unordered_set<int> stack1;//Node id
-    list<pair<int, bool>> batch1;//Node id
-    for(int i = 0; i < points.size(); i ++){
-        Point p = points[i];
-        batch1.push_back(pair<int, bool>(p.n->id, false));
-        if(i == points.size() - 1 || points[i + 1].y != p.y || points[i + 1].x != p.x){
-            for(auto &key : batch1){
-                if(stack1.erase(key.first)){
-                    key.second = true;
-                }
-            }
-            for(auto &key : batch1){
-                if(!key.second){
-                    for(auto key2 : stack1){
-                        Node * n2 = nodes[key2];
-                        n2->connections.insert(nodes[key.first]);
-                        nodes[key.first]->connections.insert(n2);
-                    }
-                    stack1.insert(key.first);
-                }
-            }
-            batch1.clear();
-        }
-        if(points[i + 1].x != p.x || i == points.size() - 1){
-            stack1.clear();
-        }
-    }
-
 
     sort(points.begin(), points.end(), [](const Point p1, const Point p2){
         if(p1.y != p2.y)
@@ -139,44 +104,84 @@ int main(){
         }
     }
 
+
+    
+    sort(points.begin(), points.end(), [](const Point p1, const Point p2){
+        if(p1.x != p2.x)
+            return p1.x < p2.x;
+        return p1.y < p2.y;
+    });
+    unordered_set<int> stack1;//Node id
+    list<pair<int, bool>> batch1;//Node id
+    for(int i = 0; i < points.size(); i ++){
+        Point p = points[i];
+        batch1.push_back(pair<int, bool>(p.n->id, false));
+        if(i == points.size() - 1 || points[i + 1].y != p.y || points[i + 1].x != p.x){
+            for(auto &key : batch1){
+                if(stack1.erase(key.first)){
+                    key.second = true;
+                }
+            }
+            for(auto &key : batch1){
+                if(!key.second){
+                    for(auto key2 : stack1){
+                        Node * n2 = nodes[key2];
+                        n2->connections.insert(nodes[key.first]);
+                        nodes[key.first]->connections.insert(n2);
+                    }
+                    stack1.insert(key.first);
+                }
+            }
+            batch1.clear();
+        }
+        if(points[i + 1].x != p.x || i == points.size() - 1){
+            stack1.clear();
+        }
+    }
+    //sweeping line
+    
     points.clear();
 
-    Node * startingNode = nullptr;
-    Node * endingNode = nullptr;
-
+    unordered_map<int, map< int, vector<Node *> >> sweepPoints;
     for(auto n : nodes){
-        if(startPointX >= n->lowX && startPointX <= n->highX &&
-         startPointY >= n->lowY && startPointY <= n->highY){
-             //match
-            startingNode = n;
-         }
-        if(endPointX >= n->lowX && endPointX <= n->highX &&
-         endPointY >= n->lowY && endPointY <= n->highY){
-             //match
-            endingNode = n;
-         }
+        if(sweepPoints.find(n->lowX) == sweepPoints.end()){
+            sweepPoints[n->lowX] = map< int, vector<Node *> >();
+        }
+        if(sweepPoints.find(n->highX) == sweepPoints.end()){
+            sweepPoints[n->highX] = map< int, vector<Node *> >();
+        }
+        if(sweepPoints[n->lowX].find(n->lowY) == sweepPoints[n->lowX].end()){
+            sweepPoints[n->lowX][n->lowY] = vector<Node *>();
+        }
+        if(sweepPoints[n->lowX].find(n->highY) == sweepPoints[n->lowX].end()){
+            sweepPoints[n->lowX][n->highY] = vector<Node *>();
+        }
+        if(sweepPoints[n->highX].find(n->lowY) == sweepPoints[n->highX].end()){
+            sweepPoints[n->highX][n->lowY] = vector<Node *>();
+        }
+        if(sweepPoints[n->highX].find(n->highY) == sweepPoints[n->highX].end()){
+            sweepPoints[n->highX][n->highY] = vector<Node *>();
+        }
+        sweepPoints[n->lowX][n->lowY].push_back(n);
+        sweepPoints[n->lowX][n->highY].push_back(n);
+        sweepPoints[n->highX][n->lowY].push_back(n);
+        sweepPoints[n->highX][n->highY].push_back(n);
     }
-
-    //brute force O(n^2)
+    unordered_map<int, vector<int> > dangers;
     for(int i = 0; i < pointsCount; i ++){
         int pointX, pointY;
         cin >> pointX >> pointY;
-        for(auto it = nodes.begin(); it != nodes.end();++it){
-            auto n = *it;
-            if(pointX >= n->lowX && pointX <= n->highX 
-                && pointY >= n->lowY && pointY <= n->highY){
-                //match
-                for(auto it = n->connections.begin(); it != n->connections.end();){
-                    Node * connected = *it;
-                    ++it;
-                    connected->connections.erase(n);
-                    n->connections.erase(connected);
-                }
-                break;
-            }
+        if(dangers.find(pointX) == dangers.end()){
+            dangers[i] = vector<int>();
         }
+        dangers[i].push_back(pointX);
+        dangers[i].push_back(pointY);
     }
 
-    bfs(startingNode, endingNode, nodesCount);
-    cout<<endingNode->pathFromSource + 1;
+    map<int, vector<Node *>> currLayer;
+    
+    //bfs(startingNode, endingNode, nodesCount);
+    //cout<<endingNode->pathFromSource + 1;
+
+    return 0;
 }
